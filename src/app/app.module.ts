26c0fe16b566ac75a0220da50470a2d9287fa641
QUYEN LY <quyen.ly.151@outlook.com>
2021-05-29 13:36:17 +0700

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BookStoreComponent } from './book-store/book-store.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookItemComponent } from './components/book-item/book-item.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookStoreComponent,
+    BookListComponent,
+    BookItemComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
